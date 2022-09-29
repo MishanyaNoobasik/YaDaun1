7ba77ba925be3a5384e94e09518a0134a4a58c83
MishanyaNoobasik <mixafonin@gmail.com>
2022-09-29 20:20:12 +0300

    почти...

--- test_git.py
+++ test_git.py
@@ -1,1 +1,26 @@
-# Гоша нуб.
+def valid_braces(string):
+    if len(string) % 2 == 0:
+        d = {'(': ')', '{': '}', '[': ']'}
+        a = len(string) / 2
+        sstring = []
+        for j in range(len(string)):
+            sstring.append(string[j])
+        print(sstring)
+        for i in range(int(a)):
+            if sstring[i] == d.get(sstring[i + 1]):
+                sstring.pop(i)
+                sstring.pop(i)
+            else:
+                pass
+        print(sstring)
+        if len(sstring) > 0:
+            res = False
+        else:
+            res = True
+    else:
+        res = False
+    return res
+
+
+aa = '()'
+print(valid_braces(aa))
