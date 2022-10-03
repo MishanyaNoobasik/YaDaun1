00550153dfda094033ac7f3b89eed08d6947da38
MishanyaNoobasik <mixafonin@gmail.com>
2022-10-03 09:01:21 +0300

    ebashu

--- cw2.py
+++ cw2.py
@@ -1,10 +1,2 @@
-def get_middle(s):
-    if len(s) % 2 == 1:
-        res = s[int(abs(len(s)/2))]
-    else:
-        res = s[int(len(s)/2) - 1] + s[int(len(s)/2)]
-    return res
-
-
-a = '1ff1'
-print(get_middle(a))
+def top_3_words(text):
+    return
